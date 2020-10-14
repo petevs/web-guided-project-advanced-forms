d7f1545c24c4ffe5f8e29ce5f435fcc3969d750b
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-14 12:18:01 -0500

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -4,11 +4,12 @@ import * as yup from "yup";
 export default yup.object().shape({
   username: yup
     .string()
-    .required("username is required").min(3, "username must be 3 character"),
+    .required("username is required")
+    .min(3, "username must be 3 character"),
   email: yup
-  .string(),
-  
-  .required('must be a valid email address'),
+    .string()
+    .email("must be a valid email address")
+    .required("email is required"),
   role: yup.string(),
   civil: yup.string(),
   coding: yup.boolean(),
