9534a48776873bbadafcd8e2423750e7ebdef139
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-14 12:44:16 -0500

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -93,7 +93,7 @@ export default function App() {
       role: formValues.role.trim(),
       civil: formValues.civil.trim(),
       // 🔥 STEP 7- WHAT ABOUT HOBBIES?
-      hobbies: 
+      hobbies: ['coding', 'reading', 'hiking'].filter
     };
     // 🔥 STEP 8- POST NEW FRIEND USING HELPER
   };
