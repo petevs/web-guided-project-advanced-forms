dd92848358af72b40a6d53c9aeffdb6d0fd0ad1c
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-14 11:21:29 -0500

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -1,34 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function FriendForm(props) {
-  const {
-    values,
-    submit,
-    change,
-    disabled,
-    errors,
-  } = props
+  const { values, submit, change, disabled, errors } = props;
 
-  const onSubmit = evt => {
-    evt.preventDefault()
-    submit()
-  }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
 
-  const onChange = evt => {
+  const onChange = (evt) => {
     /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
-    const { name, value } = evt.target
-    change(name, value)
-  }
+    const { name, value } = evt.target;
+    change(name, value);
+  };
 
   return (
-    <form className='form container' onSubmit={onSubmit}>
-      <div className='form-group submit'>
+    <form className="form container" onSubmit={onSubmit}>
+      <div className="form-group submit">
         <h2>Add a Friend</h2>
 
         {/* 🔥 DISABLE THE BUTTON */}
         <button>submit</button>
 
-        <div className='errors'>
+        <div className="errors">
           {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
           <div>{errors.username}</div>
           <div>{errors.email}</div>
@@ -37,83 +31,74 @@ export default function FriendForm(props) {
         </div>
       </div>
 
-      <div className='form-group inputs'>
+      <div className="form-group inputs">
         <h4>General information</h4>
 
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
-        <label>Username&nbsp;
+        <label>
+          Username&nbsp;
           <input
             value={values.username}
             onChange={onChange}
-            name='username'
-            type='text'
+            name="username"
+            type="text"
           />
         </label>
 
-        <label>Email
+        <label>
+          Email
           <input
             value={values.email}
             onChange={onChange}
-            name='email'
-            type='text'
+            name="email"
+            type="text"
           />
         </label>
 
         {/* ////////// DROPDOWN ////////// */}
         {/* ////////// DROPDOWN ////////// */}
         {/* ////////// DROPDOWN ////////// */}
-        <label>Role
-          <select
-            onChange={onChange}
-            value={values.role}
-            name='role'
-          >
-            <option value=''>- Select an option -</option>
-            <option value='student'>Student</option>
-            <option value='alumni'>Alumni</option>
-            <option value='instructor'>Instructor</option>
-            <option value='tl'>Team Lead</option>
+        <label>
+          Role
+          <select onChange={onChange} value={values.role} name="role">
+            <option value="">- Select an option -</option>
+            <option value="student">Student</option>
+            <option value="alumni">Alumni</option>
+            <option value="instructor">Instructor</option>
+            <option value="tl">Team Lead</option>
           </select>
         </label>
 
         {/* ////////// RADIO BUTTONS ////////// */}
         {/* ////////// RADIO BUTTONS ////////// */}
         {/* ////////// RADIO BUTTONS ////////// */}
-        <label>Single
+        <label>
+          Single
           <input
-           type="radio"
-           name="civil"
-           value={}
-           checked={true}
+            type="radio"
+            name="civil"
+            value={values.civil}
+            checked={true}
           />
-
         </label>
 
-        <label>Married
-
-        </label>
+        <label>Married</label>
       </div>
 
-      <div className='form-group checkboxes'>
+      <div className="form-group checkboxes">
         <h4>Hobbies</h4>
 
         {/* ////////// CHECKBOXES ////////// */}
         {/* ////////// CHECKBOXES ////////// */}
         {/* ////////// CHECKBOXES ////////// */}
-        <label>Hiking
+        <label>Hiking</label>
 
-        </label>
+        <label>Reading</label>
 
-        <label>Reading
-
-        </label>
-
-        <label>Coding
-
-        </label>
+        <label>Coding</label>
       </div>
     </form>
-  )
+  );
 }
