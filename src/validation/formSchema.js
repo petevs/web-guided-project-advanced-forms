a8cc10334484fd9eddccd373ffc2f129e6e9e797
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-14 12:22:46 -0500

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -10,20 +10,22 @@ export default yup.object().shape({
     .string()
     .email("must be a valid email address")
     .required("email is required"),
-  role: yup.string().oneOf(["tl", "instructor", "alumni", "student"]),
+  role: yup
+    .string()
+    .oneOf(["tl", "instructor", "alumni", "student"], "role is required"),
   civil: yup.string().oneOf(["married", "single"], "Civil status is required"),
   coding: yup.boolean(),
   reading: yup.boolean(),
   hiking: yup.boolean(),
 });
 
-const formSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Must be a valid email address.")
-    .required("Must include email address."),
-  password: Yup.string()
-    .required("Password is Required")
-    .min(6, "Passwords must be at least 6 characters long."),
-  terms: Yup.boolean().oneOf([true], "You must accept Terms and Conditions"),
-  // required isn't required for checkboxes.
-});
+// const formSchema = Yup.object().shape({
+//   email: Yup.string()
+//     .email("Must be a valid email address.")
+//     .required("Must include email address."),
+//   password: Yup.string()
+//     .required("Password is Required")
+//     .min(6, "Passwords must be at least 6 characters long."),
+//   terms: Yup.boolean().oneOf([true], "You must accept Terms and Conditions"),
+//   // required isn't required for checkboxes.
+// });
